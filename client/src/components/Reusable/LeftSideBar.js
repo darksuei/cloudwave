739f596527ea1eac64b4135fa1e498dd71ca6335
Suei43 <chrisraphael576@gmail.com>
2023-08-09 14:45:06 +0100

    Fix: refactored navbar

--- client/src/components/Reusable/LeftSideBar.js
+++ client/src/components/Reusable/LeftSideBar.js
@@ -8,15 +8,15 @@ export default function LeftSideBar() {
         <ul className="flex flex-col w-full relative">
           <Avatar />
           <div>
-            <a href='/'><li className='bg-blue-900 px-12 py-4 flex items-center gap-x-2 rounded-r-2xl'><i className="fas fa-home"></i>Home</li></a>
-            <a href='/shared'><li className='px-12 py-4 flex items-center gap-x-2'><i className="fas fa-share-alt"></i>Shared</li></a>
-            <a href='/favorites'><li className='px-12 py-4 flex items-center gap-x-2'><i className="fas fa-star"></i>Favorites</li></a>
-            <a href='/upload'><li className='px-12 py-4 flex items-center gap-x-2'><i className="fas fa-upload"></i>Upload Files</li></a>
+            <a href='/'><li className='bg-blue-900 px-12 py-4 flex items-center gap-x-2 rounded-r-2xl'><div className='p-1 w-5'><i className="fas fa-home"></i></div>Home</li></a>
+            <a href='/shared'><li className='px-12 py-4 flex items-center gap-x-2'><div className='p-1 w-5'><i className="fas fa-share-alt"></i></div>Shared</li></a>
+            <a href='/favorites'><li className='px-12 py-4 flex items-center gap-x-2'><div className='p-1 w-5'><i className="fas fa-star"></i></div>Favorites</li></a>
+            <a href='/upload'><li className='px-12 py-4 flex items-center gap-x-2'><div className='p-1 w-5'><i className="fas fa-upload"></i></div>Upload Files</li></a>
           </div>
           <div className='rounded-r-3xl absolute bottom-3.5'>
             {/* <a href='/login'><li className='px-14 py-4 flex items-center gap-x-2'><i className="fas fa-sign-in-alt"></i>Login</li></a> */}
-            <a href='/settings'><li className='px-14 py-4 flex items-center gap-x-2'><i className="fas fa-cog"></i>Settings</li></a>
-            <a href='/logout'><li className='px-14 py-4 flex items-center gap-x-2'><i className="fas fa-sign-out-alt"></i>Logout</li></a>
+            <a href='/settings'><li className='px-14 py-4 flex items-center gap-x-2'><div className='p-1 w-5'><i className="fas fa-cog"></i></div>Settings</li></a>
+            <a href='/logout'><li className='px-14 py-4 flex items-center gap-x-2'><div className='p-1 w-5'><i className="fas fa-sign-out-alt"></i></div>Logout</li></a>
           </div>
         </ul>
       </nav>
