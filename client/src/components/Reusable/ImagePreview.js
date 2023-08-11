9ad1dcd7d56c604aa4858c1f700ddbc9c79391e5
Suei43 <chrisraphael576@gmail.com>
2023-08-11 13:57:16 +0100

    Fix: Dropdown menu bug

--- client/src/components/Reusable/ImagePreview.js
+++ client/src/components/Reusable/ImagePreview.js
@@ -1,22 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 
 export default function ImagePreview({ imageUrl,fileCategory, uploadDate }){
-
+  const [dropDown, setDropDown] = useState(false);
+  function toggleDropDown(e){
+    e.stopPropagation();
+    setDropDown(!dropDown);
+  };
+  useEffect(() => {
+    function handleDocumentClick() {
+      setDropDown(false);
+    }
+    document.body.addEventListener("click", handleDocumentClick);
+  }, []);
   return (
     <div className="flex flex-col w-full h-4/5">
         <div className="relative h-full flex items-center justify-center">
           <img src={imageUrl} alt="Preview" style={{ objectFit: 'cover', width: '100%', height: '100%', borderRadius: '0.5rem' }}/>
         </div>
         <div className='flex flex-row justify-between'>
-          <div className='flex flex-col gap-y-3 p-3'>
+          <div className='flex flex-col gap-y-3 py-3'>
             <h1 className="text-blue-500 font-black text-lg">IMG_1</h1>
             <p className="text-xs text-gray-400">{uploadDate}</p>
             <div className='text-sm bg-emerald-100 text-emerald-500 flex items-center rounded-xl px-4'>{fileCategory}</div>
           </div>
-          <div className='flex flex-row gap-x-3 p-3'>
+          <div className='flex flex-row gap-x-3 py-3'>
             <i className="fas fa-share-alt text-blue-700 cursor-pointer h-fit p-1.5 rounded-full hover:bg-slate-200"></i>
             <i className="fas fa-trash text-blue-700 cursor-pointer h-fit p-1.5 rounded-full hover:bg-slate-200"></i>
-            <i className="fas fa-ellipsis-v text-blue-700 cursor-pointer h-fit p-1.5 rounded-full hover:bg-slate-200"></i>
+            <i className="fas fa-ellipsis-v text-blue-700 cursor-pointer h-fit p-1.5 rounded-full hover:bg-slate-200" onClick={(e)=>toggleDropDown(e)}></i>
+            {dropDown && (
+                                <div className="origin-top-right absolute bottom-0 right-6 mt-2 w-36 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-50">
+                                    <div class="py-1 flex flex-col" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                                        <button class="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:bg-slate-100 w-full flex flex-row justify-between items-center border-b" role="menuitem">
+                                            <span>Download</span>
+                                            <i class="fas fa-download text-xs"></i>
+                                        </button>
+                                        <button class="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:bg-slate-100 w-full flex flex-row justify-between items-center" role="menuitem">
+                                            <span>Rename</span>
+                                            <i class="fas fa-edit text-xs"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                              )}
           </div>
         </div>
     </div>
--- client/src/components/Reusable/Recent.js
+++ client/src/components/Reusable/Recent.js
@@ -4,31 +4,33 @@ import { useState, useEffect } from 'react';
  
 
 export default function Recent({title}){
-    const initialDropdownStates = Array(Object.values(data).length).fill(false);
-    const [dropdownStates, setDropdownStates] = useState(initialDropdownStates);
+    const [dropdownState, setDropdownState] = useState([]);
 
     useEffect(() => {
-        function handleDocumentClick(event) {
-          setDropdownStates(prevStates => prevStates.map(() => false));
+        function handleDocumentClick() {
+          setDropdownState([]);
         }
-        document.addEventListener("click", handleDocumentClick);
+        document.body.addEventListener("click", handleDocumentClick);
     
         return () => {
           document.removeEventListener("click", handleDocumentClick);
         };
       }, []);
     
-      function handleDropdownClick(index) {
-        setDropdownStates(prevStates =>
-          prevStates.map((prevState, i) => (i === index ? !prevState : false))
-        );
+      function handleDropdownClick(index,e) {
+        e.stopPropagation();
+        if (dropdownState.includes(index)) {
+            setDropdownState(dropdownState.filter(itemIndex => itemIndex !== index));
+            } else {
+            setDropdownState([...dropdownState, index]);
+            } 
       }
 
     return(
         <div className=' flex gap-y-4 flex-col p-12 py-4 w-full'>
             <h1 className='text-blue-700 text-xl font-extrabold'>Recently {title}</h1>
             <div className='flex flex-col gap-y-2.5'>
-            {Object.values(data).map((item) => {
+            {Object.values(data).map((item,idx) => {
                 return(
                     <div className='flex flex-row bg-white p-2.5 rounded-xl items-center gap-x-1.5 pr-4 relative cursor-pointer'>
                         <div className='bg-indigo-500 p-2 rounded-lg w-9 h-9 flex items-center justify-center'><i className="fas fa-image text-white text-sm"></i></div>
@@ -41,15 +43,25 @@ export default function Recent({title}){
                             <button
                                 type="button"
                                 className="inline-flex items-center p-1 focus:outline-none"
-                                onClick={() => setDropdownStates(!dropdownStates)}
+                                onClick={(e) => handleDropdownClick(item,e)}
                             >
                                 <i className="fas fa-ellipsis-h text-indigo-500 text-lg"></i>
                             </button>
-                            {dropdownStates && (
-                                <div className="origin-top-right absolute right-0 mt-2 w-36 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-                                    <div class="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-                                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Delete</a>
-                                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Download</a>
+                            {dropdownState.includes(item) && (
+                                <div className="origin-top-right absolute right-0 mt-2 w-36 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-50">
+                                    <div class="py-1 flex flex-col" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                                        <button class="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:bg-slate-100 w-full flex flex-row justify-between items-center border-b" role="menuitem">
+                                            <span>Download</span>
+                                            <i class="fas fa-download text-xs"></i>
+                                        </button>
+                                        <button class="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:bg-slate-100 w-full flex flex-row justify-between items-center border-b" role="menuitem">
+                                            <span>Rename</span>
+                                            <i class="fas fa-edit text-xs"></i>
+                                        </button>
+                                        <button class="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:bg-slate-100 w-full flex flex-row justify-between items-center" role="menuitem">
+                                            <span>Delete</span>
+                                            <i class="fas fa-trash text-xs"></i>
+                                        </button>
                                     </div>
                                 </div>
                             )}
