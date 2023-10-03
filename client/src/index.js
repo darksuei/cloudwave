363178563cfc8031990974d0e4c67b47a06ab3ed
darksuei <chrisraphael576@gmail.com>
2023-10-03 05:16:58 +0100

    Add service worker

--- client/src/index.js
+++ client/src/index.js
@@ -7,5 +7,16 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/service-worker.js")
+    .then((registration) => {
+      console.log("Service Worker registered with scope:", registration.scope);
+    })
+    .catch((error) => {
+      console.error("Service Worker registration failed:", error);
+    });
+}
---
+++ client/workbox-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  globDirectory: "build/",
+  globPatterns: ["**/*.{html,js,css,png,jpg,svg}"],
+  swDest: "build/service-worker.js",
+};
