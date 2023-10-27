2e9d4cddd432e8c6135531225df1aba9f161e9ee
darksuei <chrisraphael576@gmail.com>
2023-10-27 08:10:32 +0100

    Dir tree update

--- server/utils/errorHandler.js
+++
@@ -1,9 +0,0 @@
-class errorHandler extends Error {
-  constructor(message, statusCode) {
-    super(message);
-    this.statusCode = statusCode;
-    Error.captureStackTrace(this, this.constructor);
-  }
-}
-
-module.exports = errorHandler;
