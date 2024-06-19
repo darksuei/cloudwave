48594ae6f72b8103e0f74a6454ba35e250ef0638
darksuei <chrisraphael576@gmail.com>
2024-06-19 13:33:41 +0100

    Fix: Percentage used bar

--- client/src/components/Reusable/Storage.js
+++ client/src/components/Reusable/Storage.js
@@ -9,6 +9,7 @@ export function Storage({ width }) {
   const [usedSpace, setUsedSpace] = useState(0);
   const [unit, setUnit] = useState("MB");
   const [spaceLeft, setSpaceLeft] = useState(100);
+  const [percentageUsed, setPercentageUsed] = useState("0%");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,6 +18,7 @@ export function Storage({ width }) {
         setUsedSpace(filesData[0]);
         setUnit(filesData[1]);
         setSpaceLeft(100 - filesData[2]);
+        setPercentageUsed(String(filesData[2]) + "%");
       } catch (error) {
         console.error("Error fetching storage:", error);
       }
@@ -30,44 +32,28 @@ export function Storage({ width }) {
 
   async function getFiles(authToken) {
     try {
-      const response = await axios.get(
-        `${process.env.REACT_APP_SERVER_URL}/api/storage`,
-        {
-          headers: {
-            Authorization: `Bearer ${authToken}`,
-          },
-        }
-      );
-      return [
-        response.data.storageUsed,
-        response.data.unit,
-        response.data.percentage,
-      ];
+      const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/storage`, {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+      return [response.data.storageUsed, response.data.unit, response.data.percentage];
     } catch (error) {
       console.error("Files error:", error);
     }
   }
 
-  useEffect(() => {
-    const loadingBar = document.querySelector(".loading-bar");
-    loadingBar.style.width = `${30}%`;
-  }, []);
-
   return (
-    <div
-      className={`flex flex-col bg-gray-200 rounded-xl noSelect ${
-        width ? width : "w-full"
-      } p-3 gap-y-3`}
-    >
-      <div className="text-sm flex flex-row justify-between">
-        <h1 className="text-blue-500 font-extrabold text-xs">Your Storage</h1>
-        <p className="text-emerald-500 text-xs">{spaceLeft}% left</p>
+    <div className={`flex flex-col bg-gray-200 rounded-xl noSelect ${width ? width : "w-full"} p-3 gap-y-3`}>
+      <div className='text-sm flex flex-row justify-between'>
+        <h1 className='text-blue-500 font-extrabold text-xs'>Your Storage</h1>
+        <p className='text-emerald-500 text-xs'>{spaceLeft}% left</p>
       </div>
-      <div className="text-xs text-blue-500">
+      <div className='text-xs text-blue-500'>
         {usedSpace} {unit} OF 3 GB USED
       </div>
       <div className={`loading-bar-container ${!usedSpace && "loading"}`}>
-        <div className="loading-bar"></div>
+        <div className='loading-bar' style={{ width: percentageUsed }}></div>
       </div>
     </div>
   );
